86b213c5db8f3ef95d3bcd54710a7b23aa797ca9
kostya05983 <kostya05983@mail.ru>
2019-05-11 17:56:03 +0700

    [ADD] new tasks

---
+++ book/chapter_3/3_1.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	width, height = 600, 320
+	cells         = 100
+	xyrange       = 30.0
+	xyscale       = width / 2 / xyrange
+	zscale        = height * 0.4
+	angle         = math.Pi / 6
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) //sin(30) cos(30)
+
+func main() {
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+1)
+			if ax < math.MaxFloat64/2 && ax > -math.MaxFloat64/2 &&
+				ay < math.MaxFloat64/2 && ay > -math.MaxFloat64/2 &&
+				bx < math.MaxFloat64/2 && bx > -math.MaxFloat64/2 &&
+				by < math.MaxFloat64/2 && by > -math.MaxFloat64/2 &&
+				cx < math.MaxFloat64/2 && cx > -math.MaxFloat64/2 &&
+				cy < math.MaxFloat64/2 && cy > -math.MaxFloat64/2 &&
+				dx < math.MaxFloat64/2 && dx > -math.MaxFloat64/2 &&
+				dy < math.MaxFloat64/2 && dy > -math.MaxFloat64/2 {
+				fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+					ax, ay, bx, by, cx, cy, dx, dy)
+			}
+		}
+	}
+	fmt.Printf("</svg>")
+}
+
+func corner(i, j int) (float64, float64) {
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	z := f(x, y)
+
+	sx := width/2 + (x*y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyrange - z*zscale
+	return sx, sy
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y)
+	return math.Sin(r)
+}
